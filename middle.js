74f4ef74169ba24aab80c983c95c69312cd82afe
Your Name <your@email.com>
2023-04-05 10:26:00 -0600

    implementation extract of middle elements of an array

---
+++ middle.js
@@ -0,0 +1,51 @@
+const getArguments = function () {
+  const args = JSON.parse(process.argv.slice(2));
+  console.log('args:', args);
+
+  return args
+}
+
+const assertArraysEqual = function(arg1, arg2){
+  const rights = String.fromCodePoint(0x2705);
+  const wrong = String.fromCodePoint(0x274C);
+
+  if (eqArrays(arg1, arg2)){
+    console.log(`${rights} The argument ${arg1} is exactly the same as ${arg2}`)
+  }else {
+    console.log(`${wrong} The argument ${arg1} is different to ${arg2}`)
+  }
+};
+
+
+const eqArrays = function(array1, array2){
+  return JSON.stringify(array1) === JSON.stringify(array2)
+} 
+
+const middle = function(array){
+
+  let middleArray = [];
+  let index = 0
+  console.log(array.length)
+  if (array.length <= 2){
+    return middleArray
+  }else if ( array.length % 2 === 0){
+    index = array.length/2
+    middleArray.push(array[index - 1])
+    middleArray.push(array[index ])  
+    return middleArray 
+
+  }else {
+    index = (array.length + 1)/2
+    middleArray.push(array[index -1])
+    return middleArray
+  }
+
+
+}
+const result = middle(getArguments())
+console.log(result)
+
+// console.log(middle([1, 2, 3, 4]))
+// console.log(middle([1, 2, 3, 4, 5, 6]))
+// console.log(middle([1, 2]))
+// console.log(middle([1, 2, 3, 4]))
