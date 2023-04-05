a34f6aae7073ad6073ca49bbdf3fa072b178c224
Your Name <your@email.com>
2023-04-05 14:30:18 -0600

    implementation of loop in objects

---
+++ countOnly.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  const rights = String.fromCodePoint(0x2705);
+  const wrong = String.fromCodePoint(0x274C);
+
+  if (actual === expected) {
+    console.log(`${rights} Assertion Passed: ${actual} === ${expected}`);
+    
+  } else {
+    console.log(`${wrong} Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems){
+    if (itemsToCount[item]){
+      console.log(item)
+      if (results[item]){
+      results[item] += 1
+      }else {
+      results[item] = 1
+      }
+    }
+  }
+  console.log(results)
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
