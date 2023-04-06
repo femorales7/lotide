0453e967e9663afc53146fb2c6c26043f3b09b83
Your Name <your@email.com>
2023-04-06 14:22:31 -0600

    implementation of map function with callback

---
+++ map.js
@@ -0,0 +1,30 @@
+const eqArrays = function(array1, array2){
+  return JSON.stringify(array1) === JSON.stringify(array2)
+}  
+
+const assertArraysEqual = function(arg1, arg2){
+  const rights = String.fromCodePoint(0x2705);
+  const wrong = String.fromCodePoint(0x274C);
+
+  if (eqArrays(arg1, arg2)){
+    console.log(`${rights} The argument ${arg1} is exactly the same as ${arg2}`)
+  }else {
+    console.log(`${wrong} The argument ${arg1} is different to ${arg2}`)
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback){
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  
+ 
+  }
+  return results;
+}
+const result1 = map(words, word => word[0]);
+console.log(result1)
+
+
+assertArraysEqual(result1, [ 'g', 'c', 't', 'm', 't', 6 ]);
