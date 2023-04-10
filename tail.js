b89d9a53c6e02d68005a1ae4abd189a4b82a1918
Your Name <your@email.com>
2023-04-10 10:08:03 -0600

    refactor tail file calling export function for use tailTest

--- tail.js
+++ tail.js
@@ -1,21 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const rights = String.fromCodePoint(0x2705);
-  const wrong = String.fromCodePoint(0x274C);
-  if (actual === expected) {
-    console.log(`${rights} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${wrong} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const head = function(array) {
-  if (array.length === 0) {
-    return undefined;
-  } else {
-    return array[0];
-  }
-};
-
 const tail = function(array) {
   let newArray = [];
 
@@ -30,14 +12,4 @@ const tail = function(array) {
   }
  
 };
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(words.length, 3);
-
-assertEqual(words.length, 2);
-assertEqual(words[0], "Lighthouse");
-assertEqual(words[1], "Labs");
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = tail;
